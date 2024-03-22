7c53d3deaee04031a2992d36dc7b129b38368425
Wojciech Jozwik <wj.telefon@gmail.com>
2024-03-22 21:04:04 +0100

    Update NewFurniture styles with RWD

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -61,7 +61,7 @@ const NewFurniture = ({ categories, products }) => {
         </div>
         <div className='row'>
           {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-            <div key={item.id} className='col-3'>
+            <div key={item.id} className='col-6 col-md-4 col-lg-3'>
               <ProductBox {...item} />
             </div>
           ))}
