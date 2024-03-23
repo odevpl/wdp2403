dc179c7c45b6a68483324da14c1a08debb234817
Wojciech Jozwik <wj.telefon@gmail.com>
2024-03-23 12:34:56 +0100

    Update FeatureBoxes styles with RWD

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -15,25 +15,25 @@ const FeatureBoxes = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row'>
-        <div className='col'>
+        <div className='col-6 col-lg-3'>
           <FeatureBox icon={faTruck} active>
             <h5>Free shipping</h5>
             <p>All orders</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-6 col-lg-3'>
           <FeatureBox icon={faHeadphones}>
             <h5>24/7 customer</h5>
             <p>support</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-6 col-lg-3'>
           <FeatureBox icon={faReplyAll}>
             <h5>Money back</h5>
             <p>guarantee</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-6 col-lg-3'>
           <FeatureBox icon={faBullhorn}>
             <h5>Member discount</h5>
             <p>First order</p>
