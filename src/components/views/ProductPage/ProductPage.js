ecffb7fc5c7181718092926591b37013b681510e
Jakub Stelmasiak <stelmon3@gmail.com>
2024-03-24 15:16:16 +0100

    add splash

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,7 +2,22 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+const ProductPage = () => {
+  return (
+    <div className='container'>
+      <div className={styles.root}>
+        <div className={styles.splash}>
+          <div className={styles.headline}>
+            <span>
+              BEDROOM <strong>FURNITURE</strong>
+            </span>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // ProductPage.propTypes = {};
 
