a5f20ec646a082885459ceaaf2785a54ca5665ca
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-03-25 13:57:46 +0100

    Add New Forniture to Product page

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,14 +2,24 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 import ProductCard from '../ProductCard/ProductCard';
+import NewFurniture from '../../features/NewFurniture/NewFurniture';
+import { useSelector } from 'react-redux';
+import { getAllCategories } from '../../../redux/categoriesRedux';
+import { getAllProducts } from '../../../redux/productsRedux';
+
 
 const ProductPage = () => {
   return (
     <div className={styles.root}>
+
       <ProductCard />
+      <NewFurniture />
     </div>
   );
 };
+
+
+
 // ProductPage.propTypes = {};
 
 export default ProductPage;
