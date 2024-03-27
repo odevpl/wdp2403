49d78bc4d0872bc655ed6bf6898f28aa70f95a94
Jakub Stelmasiak <stelmon3@gmail.com>
2024-03-27 12:17:24 +0100

    final

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,22 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => {
-  return (
-    <div className='container'>
-      <div className={styles.root}>
-        <div className={styles.splash}>
-          <div className={styles.headline}>
-            <span>
-              BEDROOM <strong>FURNITURE</strong>
-            </span>
-            <p></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
 
 // ProductPage.propTypes = {};
 
