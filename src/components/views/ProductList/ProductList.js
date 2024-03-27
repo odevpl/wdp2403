82e8cda4f2de87df491c9947509b2dfa216a58d9
Jakub Stelmasiak <stelmon3@gmail.com>
2024-03-27 13:53:43 +0100

    final

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,8 +1,29 @@
 import React from 'react';
+
 // import PropTypes from 'prop-types';
+
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <div>Banner</div>
+      </div>
+      <div className={styles.container}>
+        <div className='col-md-9'>ProductList</div>
+        <div className='col-md-3'>
+          <div>Filters</div>
+          <div>by categories</div>
+          <div>by color</div>
+          <div>by price</div>
+          <div>by size</div>
+        </div>
+      </div>
+      <div className={styles.container}>Brands</div>
+    </div>
+  );
+};
 
 // ProductList.propTypes = {};
 
