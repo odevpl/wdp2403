75747a5eff53b64d2528f7b6c1776e7f294f7622
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-03-28 09:58:30 +0100

    fix conflicts

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,66 +1,11 @@
-<<<<<<< HEAD
-=======
 import React, { useState } from 'react';
->>>>>>> de66faf (Add RWD for MenuBar)
 import PropTypes from 'prop-types';
-import React from 'react';
-
-import ProductSearch from '../../features/ProductSearch/ProductSearch';
-
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import styles from './MenuBar.module.scss';
-
-<<<<<<< HEAD
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
-        </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <NavLink activeClassName={styles.active} exact to='/'>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/shop/furniture'>
-                Furniture
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/shop/chair'>
-                Chair
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/shop/table'>
-                Table
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/shop/sofa'>
-                Sofa
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/shop/bedroom'>
-                Bedroom
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName={styles.active} to='/blog'>
-                Blog
-              </NavLink>
-            </li>
-          </ul>
-=======
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
 import clsx from 'clsx';
+import ProductSearch from '../../features/ProductSearch/ProductSearch';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import styles from './MenuBar.module.scss';
 
 const MenuBar = ({ children }) => {
   const [show, setShow] = useState(false);
@@ -87,31 +32,42 @@ const MenuBar = ({ children }) => {
             </button>
             <ul className={clsx(show && styles.show)} onClick={() => setShow(!show)}>
               <li>
-                <a href='#' className={styles.active}>
+                <NavLink activeClassName={styles.active} exact to='/'>
                   Home
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Furniture</a>
+                <NavLink activeClassName={styles.active} to='/shop/furniture'>
+                  Furniture
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Chair</a>
+                <NavLink activeClassName={styles.active} to='/shop/chair'>
+                  Chair
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Table</a>
+                <NavLink activeClassName={styles.active} to='/shop/table'>
+                  Table
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Sofa</a>
+                <NavLink activeClassName={styles.active} to='/shop/sofa'>
+                  Sofa
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Bedroom</a>
+                <NavLink activeClassName={styles.active} to='/shop/bedroom'>
+                  Bedroom
+                </NavLink>
               </li>
               <li>
-                <a href='#'>Blog</a>
+                <NavLink activeClassName={styles.active} to='/blog'>
+                  Blog
+                </NavLink>
               </li>
             </ul>
           </div>
->>>>>>> de66faf (Add RWD for MenuBar)
         </div>
       </div>
     </div>
