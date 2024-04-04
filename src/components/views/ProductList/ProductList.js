9cec3f2455c5159a2e68912ebcbaed0c5b841b1f
Jakub Stelmasiak <stelmon3@gmail.com>
2024-04-04 12:33:20 +0200

    container fix

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -7,20 +7,22 @@ import styles from './ProductList.module.scss';
 const ProductList = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.container}>
-        <div>Banner</div>
-      </div>
-      <div className={styles.container}>
-        <div className='col-md-9'>ProductList</div>
-        <div className='col-md-3'>
-          <div>Filters</div>
-          <div>by categories</div>
-          <div>by color</div>
-          <div>by price</div>
-          <div>by size</div>
+      <div className='container'>
+        <div className={styles.container}>
+          <div>Banner</div>
         </div>
+        <div className={styles.container}>
+          <div className='col-md-9'>ProductList</div>
+          <div className='col-md-3'>
+            <div>Filters</div>
+            <div>by categories</div>
+            <div>by color</div>
+            <div>by price</div>
+            <div>by size</div>
+          </div>
+        </div>
+        <div className={styles.container}>Brands</div>
       </div>
-      <div className={styles.container}>Brands</div>
     </div>
   );
 };
