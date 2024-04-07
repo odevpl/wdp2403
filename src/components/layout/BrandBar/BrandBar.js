3f8ab95b06f49774fa0191566ac14a6286fc489b
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-04-07 23:48:25 +0200

    Add arrow navigation to brand slider

--- src/components/layout/BrandBar/BrandBar.js
+++ src/components/layout/BrandBar/BrandBar.js
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from '../BrandBar/BrandBar.module.scss';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
+import Button from '../../common/Button/Button';
 
 const BrandBar = ({ brands }) => {
+  const [itemWidth, setItemWidth] = useState(0);
+  const [visibleStartIndex, setVisibleStartIndex] = useState(0);
+  const sliderRef = useRef(null);
+  const visibleBrands = 6;
+
+  useEffect(() => {
+    const sliderContainer = sliderRef.current;
+    if (sliderContainer) {
+      const containerWidth = sliderContainer.offsetWidth;
+      const itemWidth = containerWidth / visibleBrands;
+      setItemWidth(itemWidth);
+    }
+  }, [visibleBrands]);
+
+  const handleScroll = (e, direction) => {
+    e.preventDefault();
+    const scrollAmount = direction === 'left' ? -itemWidth : itemWidth;
+    sliderRef.current.scrollLeft += scrollAmount;
+
+    const newVisibleStartIndex =
+      direction === 'left'
+        ? Math.max(0, visibleStartIndex - visibleBrands)
+        : Math.min(brands.length - visibleBrands, visibleStartIndex + visibleBrands);
+    setVisibleStartIndex(newVisibleStartIndex);
+  };
+
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className='row'>
           <div className='col'>
             <div className={styles.brandsBar}>
-              <div className={clsx(styles.arrowBox, styles.arrowLeft)}>
+              <Button
+                className={clsx(styles.arrowBox, styles.arrowLeft)}
+                onClick={e => handleScroll(e, 'left')}
+                variant='outline'
+              >
                 <FontAwesomeIcon className={styles.icon} icon={faChevronLeft} />
+              </Button>
+              <div className={styles.slider} ref={sliderRef}>
+                <ul className={styles.brands}>
+                  {brands.map((item, index) => {
+                    const isHidden =
+                      index < visibleStartIndex ||
+                      index >= visibleStartIndex + visibleBrands;
+                    return (
+                      <li
+                        key={item.id}
+                        className={clsx(styles.brand, isHidden && styles.noActive)}
+                      >
+                        <img src={item.img} alt={item.name} />
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
-              <ul className={styles.brands}>
-                {brands.map(item => (
-                  <li key={item.id} className={styles.brand}>
-                    <img src={item.img} alt={item.name} />
-                  </li>
-                ))}
-              </ul>
-              <div className={clsx(styles.arrowBox, styles.arrowRight)}>
+              <Button
+                className={clsx(styles.arrowBox, styles.arrowRight)}
+                onClick={e => handleScroll(e, 'right')}
+                variant='outline'
+              >
                 <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
-              </div>
+              </Button>
             </div>
           </div>
         </div>
