b6995d1d771c8fdd56c72cbd8f883947b068143c
Wojciech Jozwik <wj.telefon@gmail.com>
2024-04-07 12:51:18 +0200

    Add name, prices, favorite to compare section

--- src/components/features/StickyBar/StickyBar.js
+++ src/components/features/StickyBar/StickyBar.js
@@ -7,7 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { removeCompare } from '../../../redux/productsRedux';
 import { getCompareProducts } from '../../../redux/productsRedux';
-
+import clsx from 'clsx';
+import { faStar as faHeart } from '@fortawesome/free-regular-svg-icons';
 const StickyBar = () => {
   const dispatch = useDispatch();
   const compareProducts = useSelector(state => getCompareProducts(state));
@@ -23,19 +24,44 @@ const StickyBar = () => {
     return (
       <div className={styles.compareWrapper}>
         {compareProducts.map(product => (
-          <div
-            className={styles.compareImage}
-            key={product.id}
-            style={{
-              backgroundImage: `url(${product.img})`,
-            }}
-          >
-            <Button
-              onClick={e => removehandler(e, product)}
-              className={styles.removeButton}
+          <div key={product.id} className={styles.compareProductWrapper}>
+            <div
+              className={styles.compareImage}
+              style={{ backgroundImage: `url(${product.img})` }}
             >
-              <FontAwesomeIcon icon={faTimes} />
-            </Button>
+              <Button
+                onClick={e => removehandler(e, product)}
+                className={styles.removeButton}
+              >
+                <FontAwesomeIcon icon={faTimes} />
+              </Button>
+            </div>
+            <div className={styles.compareData}>
+              <div className={styles.content}>
+                <h5>{product.name}</h5>
+              </div>
+              <div className={styles.actions}>
+                <div className={styles.outlines}>
+                  <Button variant='outline' favorite={product.favorite}>
+                    <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+                  </Button>
+                </div>
+                <div className={styles.price}>
+                  <span
+                    className={clsx(
+                      product.oldPrice === 0 && styles.active,
+                      styles.oldPrice
+                    )}
+                  >
+                    {' '}
+                    $ {product.oldPrice}
+                  </span>
+                  <Button noHover variant='small'>
+                    $ {product.price}
+                  </Button>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
         <Button>Compare</Button>
@@ -44,3 +70,4 @@ const StickyBar = () => {
 };
 
 export default StickyBar;
+// start
