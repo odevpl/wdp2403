6aa205a00efaa00cfad6c4e777d41c17ee484bfc
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-04-08 13:27:37 +0200

    Add loginPage

---
+++ src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './LoginPage.module.scss';
+
+const LoginPage = () => {
+  return (
+    <div className={styles.root}>
+      <div className='container'>Login Page</div>
+    </div>
+  );
+};
+
+export default LoginPage;
