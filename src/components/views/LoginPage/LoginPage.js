ee5f4e72565ec26de31b42af954a9c0d8c46b14b
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-04-08 13:46:07 +0200

    Add html to LoginPage

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './LoginPage.module.scss';
+import Button from '../../common/Button/Button';
 
 const LoginPage = () => {
   return (
     <div className={styles.root}>
-      <div className='container'>Login Page</div>
+      <div className='container'>
+        <div className={styles.loginWrapper}>
+          <form>
+            <div className={styles.login}>
+              <label htmlFor='login'>Email</label>
+              <input type='email' id='login' name='login'></input>
+            </div>
+            <div className={styles.password}>
+              <label htmlFor='password'>Hasło</label>
+              <input type='passwor' id='password' name='password'></input>
+            </div>
+            <div className={styles.remember}>
+              <p>Nie pamiętasz hasła?</p>
+              <a href='#'>Przypomnij hasło</a>
+            </div>
+            <Button href='/'>Zaloguj się</Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
